36ab6baadaed55f4e29bbf7dcef85bb58ac2b437
celestar05 <ccastr20@ucsc.edu>
2026-04-06 16:58:54 -0700

    Add files via upload
    Journal #1 Prompt #1 Responses

---
+++ Journal1P1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:42:34 2026
+
+@author: celes
+"""
+
+print("Full Name: Celeste Catro")
+print("Pronouns: she/her")
+print("Favorite Movie &Food: My Favorite Movie of all time is The Fast and the Furious. However, I recently watched the movie, 'Soulmate-Korean: 소울메이트', and it is now one of my Favorite movies. My two favorite foods are Green Enchiladas and Chicken Katsu Curry. ")
