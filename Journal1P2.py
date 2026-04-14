a5a1f81f71430a998e529c626a896f00cf642ceb
celestar05 <ccastr20@ucsc.edu>
2026-04-14 13:52:18 -0700

    Add files via upload
    Journal #1 Prompt #2

---
+++ Journal1P2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 16:20:32 2026
+
+@author: celes
+"""
+
+## 2) Session 2 Prompt: Write a Python program (.py file) that prints the sum  
+## of two floating point numbers, the difference between two integers, and the 
+## product of a floating point number and an integer. In each case, have the 
+## program print out the data type of the resulting answer.
+
+import numpy as np
+
+x = 5.1279e32 + 13.6653e46
+print(x)
+
+y= 16739885 - 1299745
+print(y)
+
+z= 98.3434e-26 * (np.pi/5)
+print(z)
+
